572054a8d77ca1d1ad0582b849488ecabde34a91
nokibul82 <nokibul82@gmail.com>
2024-07-17 22:20:05 +0600

    Card Feature Modified

--- routes/web.php
+++ routes/web.php
@@ -3,6 +3,5 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    $card = \App\Models\Card::find(6);
-    return view('welcome',["card" =>$card]);
+    return view('welcome');
 });
